03691e630fb687b9a7052b00b8cafd30b30af8d2
Irene Tokareva <longhorn09@gmail.com>
2019-12-05 11:29:12 -0600

    fix edit listener

--- controllers/index.js
+++ controllers/index.js
@@ -20,15 +20,15 @@ function index(req, res, next) {
   request(DARKSKY_URL, (err, response, body) => {
     let weatherJSON = JSON.parse(body);
     let temp = Math.round(weatherJSON.currently.temperature)
-    console.log('temperature ' + temp);
+    // console.log('temperature ' + temp);
 
     let mode = true
-    if('archive' in req.query) {
+    if ('archive' in req.query) {
       mode = false
     }
-    Mycelium.find({current: mode}).populate('variety').exec(
+    Mycelium.find({ current: mode }).populate('variety').exec(
       function (err, cards) {
-        console.log("found cards")
+        // console.log("found cards")
         res.render('index', {
           // users: null,
           user: req.user,
@@ -53,19 +53,23 @@ function newCard(req, res, next) {
   request(DARKSKY_URL, (err, response, body) => {
     let weatherJSON = JSON.parse(body);
     let temp = Math.round(weatherJSON.currently.temperature)
-    console.log('temperature ' + temp);
+    // console.log('temperature ' + temp);
 
     Variety.find({}, function (err, vars) {
-      console.log("found vars");
-      res.render('new', {
-        // users: null,
-        user: req.user,
-        name: req.query.name,
-        title: 'New card',
-        moment,
-        temp,
-        vars,
-        types: ['liquid culture', 'agar culture', 'slant', 'grain spawn', 'sawdust spawn', 'woodchip spawn', 'plug spawn', 'substrate', 'log', 'outdoor patch']
+      Mycelium.find({}, function (err, cards) {
+        // console.log("found vars");
+        res.render('new', {
+          // users: null,
+          user: req.user,
+          name: req.query.name,
+          title: 'New card',
+          moment,
+          temp,
+          vars,
+          cards,
+          testText: "Foo",
+          types: ['liquid culture', 'agar culture', 'slant', 'grain spawn', 'sawdust spawn', 'woodchip spawn', 'plug spawn', 'substrate', 'log', 'outdoor patch']
+        })
       })
     })
 
@@ -75,51 +79,51 @@ function newCard(req, res, next) {
 
 function create(req, res, next) {
   console.log("create starts");
-  console.table(req.body);
+  // console.table(req.body);
   req.body.user_id = req.user._id;
   req.body.current = true;
 
-  console.log("ID ", req.user._id);
+  // console.log("ID ", req.user._id);
   if (!req.body.variety) {
-    
-  console.log("NEW VARIETY");
 
-  let variety = new Variety({
-    name: req.body.new_variety,
-    latin: req.body.new_latin,
-    abbr: req.body.new_abbr
-  } );
-  variety.save(function (err) {
-    if (err) {
-      console.log("error when saving a variety");
-      console.log(err);
-    }
-    req.body.variety = variety;
-    let card = new Mycelium(req.body);
+    console.log("NEW VARIETY");
 
-    card.save(function (err) {
-    if (err) {
-      console.log("error when saving a card");
-      console.log(err);
-    }
-    res.redirect('/');
-  })
-  })
+    let variety = new Variety({
+      name: req.body.new_variety,
+      latin: req.body.new_latin,
+      abbr: req.body.new_abbr
+    });
+    variety.save(function (err) {
+      if (err) {
+        console.log("error when saving a variety");
+        console.log(err);
+      }
+      req.body.variety = variety;
+      let card = new Mycelium(req.body);
+
+      card.save(function (err) {
+        if (err) {
+          console.log("error when saving a card");
+          console.log(err);
+        }
+        res.redirect('/');
+      })
+    })
 
     console.log("NEW VARIETY - CREATED!!!");
   } else {
 
 
-  let card = new Mycelium(req.body);
+    let card = new Mycelium(req.body);
 
-  card.save(function (err) {
-    if (err) {
-      console.log("error when saving a card");
-      console.log(err);
-    }
-    res.redirect('/');
-  })
-}
+    card.save(function (err) {
+      if (err) {
+        console.log("error when saving a card");
+        console.log(err);
+      }
+      res.redirect('/');
+    })
+  }
 }
 
 
@@ -132,10 +136,10 @@ function show(req, res) {
     Mycelium.findById(req.params.id)
       .populate('variety')
       .exec(function (err, mycelium) {
-        console.log("mycelium ", mycelium);
-  
+        // console.log("mycelium ", mycelium);
+
         res.render('show', {
-  
+
           user: req.user,
           name: req.query.name,
           title: mycelium.variety.name,
@@ -153,7 +157,7 @@ function show(req, res) {
 
 }
 
-function archive(req, res){
+function archive(req, res) {
   Mycelium.findById(req.params.id, function (err, mycelium) {
     console.log("ARCHIVE");
     mycelium.current = !mycelium.current;
@@ -162,26 +166,25 @@ function archive(req, res){
     })
   })
 }
-function delMush(req, res){
+function delMush(req, res) {
   console.log("DELETE MUSHROOM");
-  Mycelium.deleteOne({_id: req.params.id}, function (err) {
+  Mycelium.deleteOne({ _id: req.params.id }, function (err) {
     res.redirect('/');
   })
 }
 
-function createLog (req, res, next) {
-  console.log("create LOG starts");
-  console.table(req.body);
-  // req.body.log = 
+function createLog(req, res, next) {
+  // console.log("create LOG starts");
+  // console.table(req.body);
 
 
   Mycelium.findById(req.params.id, function (err, mycelium) {
-    console.log("NEW STATUS LOG");
+    // console.log("NEW STATUS LOG");
 
-    mycelium.log.push({text: req.body.text});
+    mycelium.log.push({ text: req.body.text });
     // mycelium.log = req.body;
 
-    console.log("mycelium.log ", mycelium.log);
+    // console.log("mycelium.log ", mycelium.log);
 
     mycelium.save(function (err) {
       res.redirect(`/cards/${req.params.id}`);
@@ -189,27 +192,35 @@ function createLog (req, res, next) {
   })
 }
 
-function editLog(req, res){
-  console.log("EDIT");
-  console.log("log  *************")
-  console.log(req.body._id);
-  console.log("micilyun ******")
-  console.log(req.params.id);
-  
-  console.log("*****************")
+function editLog(req, res) {
+  // console.log("EDIT");
+  // console.log("*************")
+  // console.table(req.body);
+  // console.log("******")
+  // console.log(req.params.id);
+  // console.log("*****************")
+
+
   Mycelium.findById(req.params.id).populate('mycelium.log')
-  .exec(function (err, mycelium) {
-    // console.log("EDIT mycelium: ", mycelium.log);
-    console.log("LOG ID ", req.body._id);
-    mycelium.log.findById(req.body._id, function(err){
-    })
-      // console.log("LOG ID ", req.body._id);
-    
-    mycelium.log.text = req.body.text;
-    mycelium.save(function (err) {
-      res.redirect(`/cards/${req.params.id}`);
-    })}
-  )
+    .exec(function (err, mycelium) {
+      console.log("findById error ", err);
+
+      console.log("mycelium.log after populate ");
+      console.log("mycelium.log.text ", mycelium.log[req.params.idx].text);
+      mycelium.log[req.params.idx].text = req.body.text
+      // mycelium.log.findById(req.body._id, function (err) {
+      //   mycelium.log.text = req.body.text;
+      //   mycelium.save(function (err) {
+      //     res.redirect(`/cards/${req.params.id}`);
+      //   })
+      // })
+
+      mycelium.save(function (err) {
+        console.log("save error ", err);
+        res.redirect(`/cards/${req.params.id}`);
+      })
+    }
+    )
 }
 
 
--- public/javascripts/mush.js
+++ public/javascripts/mush.js
@@ -1,13 +1,27 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var elems = document.querySelectorAll('.collapsible');
-    var instances = M.Collapsible.init(elems);
-});
+//     var elems = document.querySelectorAll('.collapsible');
+//     var instances = M.Collapsible.init(elems);
+// });
+
+var elem = document.querySelector('.collapsible.expandable');
+  var instance = M.Collapsible.init(elem, {
+    accordion: false
+  });
 
 const dd = document.querySelectorAll('.text a');
-dd.forEach(function (e) {
-    e.addEventListener("click", function () {
-        document.querySelector(`.text`).classList = "hide";
-        document.querySelector(`.edit`).classList = "";
+dd.forEach(function (d) {
+    d.addEventListener("click", function (event) {
+        console.log(event.target.parentNode.parentNode);
+        let parent = event.target.parentNode.parentNode;
+        parent.querySelector(`.text`).classList = "hide";
+        parent.querySelector(`.edit`).classList = "";
     })
 })
+// document.querySelectorAll('.text a').addEventListener("click", function (e) {
+//         document.querySelector(`.text`).classList = "hide";
+//         document.querySelector(`.edit`).classList = "";
+//     })
+
+})
+
 
